e79163d658b03b6bffd9b867dfd78fb5320477f3
duy.nguyen <duy.nguyen@infis.cz>
2026-05-19 09:27:07 +0200

    nejde sušenka :(

--- CookieClickerWpf/MainWindow.xaml.cs
+++ CookieClickerWpf/MainWindow.xaml.cs
@@ -1,24 +1,40 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace CookieClickerWpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        GameState state = new GameState();
+        DispatcherTimer timer = new DispatcherTimer();
+
+        private void CookieButton_Click(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
+            state.Cookies += state.CookiesPerClick;
+            state.TotalClicks++;
+            UpdateUI();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            state.Cookies += state.CookiesPerSecond;
+            UpdateUI();
+        }
+
+        private void ShopButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Obchod není hotový.");
+        }
+
+        public void UpdateUI()
+        {
+            CookiesText.Text = state.Cookies + " sušenek";
+            PerClickText.Text = "Za kliknutí: " + state.CookiesPerClick;
+            PerSecondText.Text = "Produkce: " + state.CookiesPerSecond + " / s";
+            TotalClicksText.Text = "Kliknutí: " + state.TotalClicks;
+            TotalUpgradesText.Text = "Zakoupená vylepšení: " + state.TotalUpgradesBought;
+            TotalEarnedText.Text = "Celkem získáno: " + state.Cookies;
         }
     }
 }
